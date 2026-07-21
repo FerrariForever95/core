9b29cebf47d9165af8a70b705d8941b40326c746
M.Venkata Shanmukha Viswanadh <shanmukhamarthi@gmail.com>
2026-07-21 20:44:12 +0530

    Add draw_text8x8 and draw_bmp functions to globals

--- c modules/display/modlcd.c
+++ c modules/display/modlcd.c
@@ -941,6 +941,8 @@ static const mp_rom_map_elem_t moclcd_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_draw_rect),   MP_ROM_PTR(&moclcd_draw_rect_obj)   },
     { MP_ROM_QSTR(MP_QSTR_draw_circle), MP_ROM_PTR(&moclcd_draw_circle_obj) },
     { MP_ROM_QSTR(MP_QSTR_fill_circle), MP_ROM_PTR(&moclcd_fill_circle_obj) },
+    { MP_ROM_QSTR(MP_QSTR_draw_text8x8),MP_ROM_PTR(&moclcd_draw_text8x8_obj)},   // ADD THIS
+    { MP_ROM_QSTR(MP_QSTR_draw_bmp),    MP_ROM_PTR(&moclcd_draw_bmp_obj)    },   // ADD THIS
 };
 static MP_DEFINE_CONST_DICT(moclcd_globals, moclcd_globals_table);
 
@@ -950,3 +952,4 @@ const mp_obj_module_t mp_module_moclcd = {
 };
 
 MP_REGISTER_MODULE(MP_QSTR_moclcd, mp_module_moclcd);
+
